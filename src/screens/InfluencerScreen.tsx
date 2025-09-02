6b204b15f7dd3b87e4d5894df0c9ba0ec5e08416
Utkarsh4517 <uttkarsh.dav@gmail.com>
2025-09-03 00:59:30 +0530

    accesed avatar on influencer screen

--- src/screens/InfluencerScreen.tsx
+++ src/screens/InfluencerScreen.tsx
@@ -1,6 +1,10 @@
 import { Text, View } from 'react-native';
+import { RouteProp } from '@react-navigation/native';
+import { MainStackParamList } from '../types/navigation';
 
-export default function InfluencerScreen() {
+export default function InfluencerScreen({ route }: { route: RouteProp<MainStackParamList, 'InfluencerScreen'> }) {
+  const { avatar } = route.params;
+  console.log(avatar);
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">Influencer Screen</Text>
